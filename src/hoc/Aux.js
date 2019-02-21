ad57300978bf66add2188695d9cc0e96dcbbc2bc
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-02-20 21:15:02 -0600

    added layout component and wrapper compponent, Aux

---
+++ src/hoc/Aux.js
@@ -0,0 +1,6 @@
+// This HOC exists to wrap adjacent JSC elements in Layout.js
+const aux = (props) => props.children;
+
+export default aux;
+
+// there is no JSX used in this file, so no need to import React.
