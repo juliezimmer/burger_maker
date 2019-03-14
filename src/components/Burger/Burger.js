46f77dd07c6313009cce327c9f87fdcc40230aa2
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-14 14:42:22 -0500

    worked on build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,27 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => {
+const burger = (props) => { // ingredients are received as props
+   let transformedIngredients = Object.keys(props.ingredients)
+      .map((igKey) => {
+         console.log(igKey);
+         return [...Array(props.ingredients[igKey])]
+            .map((_, i) => {
+               return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            });
+      })
+      .reduce((arr, el) => {
+         return arr.concat(el)
+      }, []);
+   if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+   }
+   console.log(transformedIngredients);
    return ( 
       // renders a graphical representation of the burger that has top/bottom bun,meat, and cheese
       <div className={classes.Burger}>
          <BurgerIngredient type="bread-top" />
-         <BurgerIngredient type="cheese" />
-         <BurgerIngredient type="meat" />
+          {transformedIngredients}
          <BurgerIngredient type="bread-bottom" />
       </div>
    );
