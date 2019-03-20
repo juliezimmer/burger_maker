34fe03f4a21619f35898fb060a7e5ccf69eb0bcc
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-20 13:38:54 -0500

    add ingredient is working

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import classes from './BuildControls.css'; // note that the className on the div below is the same as this file that was imported with classes added: classesBuildControls
+import BuildControl from './BuildControl/BuildControl'; 
+
+const controls = [
+   { label: 'Salad', type: 'salad'},
+   { label: 'Bacon', type: 'bacon'},
+   { label: 'Cheese', type: 'cheese'},
+   { label: 'Meat', type: 'meat'}
+];
+
+// this loops through the controls array (above) using .map and puts the new array returned in an instance of BuildControl
+const buildControls = (props) => (
+   <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+         <BuildControl 
+            key={ctrl.label} 
+            label={ctrl.label}
+            added={() => props.ingredientAdded(ctrl.type)}   
+         />
+      ))}
+   </div>
+
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// all uppercase indicates a global constant
+const INGREDIENT_PRICES = {
+   salad: 0.5,
+   cheese: 0.4,
+   meat: 1.3,
+   bacon: 0.7 
+}
+
 class BurgerBuilder extends Component {
    state = {
       ingredients: {
@@ -10,13 +18,40 @@ class BurgerBuilder extends Component {
          bacon: 0,
          cheese: 0,
          meat: 0
-      }
+      },
+      totalPrice: 4 // base price w/o ingredients added
+
    }
+
+   addIngredientHandler = (type) => {
+      // need the old ingredient count
+      const oldCount = this.state.ingredients[type];
+      const updatedCount = oldCount + 1;
+      // to update ingredients, use spread operator
+      const updatedIngredients = {
+         ...this.state.ingredients, // previous ingredient state
+      };
+      updatedIngredients[type] = updatedCount;
+      const priceAddition = INGREDIENT_PRICES[type]; 
+      const oldPrice = this.state.totalPrice;
+      const newPrice = oldPrice + priceAddition;
+      this.setState({
+         totalPrice: newPrice,
+         ingredients: updatedIngredients
+      })
+   }
+
+   removeIngredientHandler = () => {
+        
+   }
+
    render() {
       return (
          <Aux>
            <Burger ingredients={this.state.ingredients}/>
-            <BuildControls />
+            <BuildControls 
+               ingredientAdded={this.addIngredientHandler}
+            />
          </Aux>
       );
    }
